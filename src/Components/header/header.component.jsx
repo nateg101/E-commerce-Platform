234115420344119df8428a4037a8e8549ba4532c
Nat Elliott-Green <natelliottgreen34@gmail.com>
2021-01-16 16:46:39 +0000

    feat: Updated mapStateToProps function in header component to utilise reselect

--- src/Components/header/header.component.jsx
+++ src/Components/header/header.component.jsx
@@ -5,6 +5,9 @@ import CartIcon from '../cart-icon/cart-icon.component';
 import CartDropDown from '../cart-dropdown/cart-dropdown.component';
 import { auth } from '../../firebase/firebase.utils';
 import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+import { selectCartHidden } from '../../Redux/cart/cart.selectors';
+import { selectCurrentUser, selectUser } from '../../Redux/user/user.selector';
 import './header.style.scss';
 
 const Header = ({ currentUser, hidden }) => (
@@ -35,9 +38,9 @@ const Header = ({ currentUser, hidden }) => (
     </div>
 )
 
-const mapStateToProps = ({ user: { currentUser }, cart: { hidden }}) => ({
-    currentUser,
-    hidden,
+const mapStateToProps = (state) => createStructuredSelector({
+    currentUser: selectCurrentUser,
+    hidden: selectCartHidden
 })
 
 export default connect(mapStateToProps)(Header);
